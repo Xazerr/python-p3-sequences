faf8d70ebeffc7625deeeb711895a3a87364ed3f
jeffreyxazer <jeffreyxazer@gmail.com>
2025-05-22 14:58:28 +0300

    Add print_fibonacci function to generate Fibonacci sequence up to length n

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
-#!/usr/bin/env python3
+def print_fibonacci(n):
+    if n <= 0:
+        print([])  # empty list for non-positive input
+        return
 
-def print_fibonacci(length):
-    pass
+    fib = [0, 1]
+
+    # If n == 1, print just [0]
+    if n == 1:
+        print([0])
+        return
+
+    while len(fib) < n:
+        # Next number is sum of last two numbers
+        next_value = fib[-1] + fib[-2]
+        fib.append(next_value)
+
+    print(fib)
